662056aa680cadba31325623c53c604779eb6e5a
Rad Cirskis <nad2000@gmail.com>
2017-02-21 15:05:08 +1300

    some useful info for dev/testing: /pyinfo

--- orcidhub-core/application.py
+++ orcidhub-core/application.py
@@ -1,5 +1,5 @@
 from requests_oauthlib import OAuth2Session
-from flask import Flask, request, redirect, session, url_for,render_template
+from flask import Flask, request, redirect, session, url_for, render_template
 from flask.json import jsonify
 import os
 from werkzeug.urls import iri_to_uri
@@ -12,6 +12,16 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+os.environ['DEBUG'] = "1"
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+#NB! Should be disabled in production
+from pyinfo import info
+@app.route('/pyinfo')
+def pyinfo():
+    return render_template('pyinfo.html', **info)
+
 db = SQLAlchemy(app)
 from authcontroller import *
 if __name__ == "__main__":
