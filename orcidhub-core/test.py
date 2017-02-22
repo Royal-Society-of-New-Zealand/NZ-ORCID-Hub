ad7749fd438744ede3801ddaa1de01f5d9e4c632
Rad Cirskis <nad2000@gmail.com>
2017-02-22 23:50:44 +1300

    simple app test

---
+++ orcidhub-core/test.py
@@ -0,0 +1,34 @@
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+import os
+from application import app
+import unittest
+import tempfile
+import json
+
+
+
+class OrcidhubTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postres:postgres@db:5432/testdb'
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def login(self, username, password):
+        pass
+
+    def test_index(self):
+        rv = self.app.get("/orcidhub/index")
+        assert b"<!DOCTYPE html>" in rv.data
+        assert b"Home" in rv.data
+
+    def test_auth(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
