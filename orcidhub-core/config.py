71d970b8a0f1dbf25e8f9a0173887129a60d6e13
Rad Cirskis <nad2000@gmail.com>
2017-03-07 11:59:21 +1300

    fixed environment orcid callback url

--- orcidhub-core/config.py
+++ orcidhub-core/config.py
@@ -10,7 +10,7 @@ token_url = 'https://pub.sandbox.orcid.org/oauth/token'
 scope = ['/authenticate']
 
 # Application redirect URL:
-redirect_uri = 'https://test.orcidhub.org.nz/auth'
+redirect_uri = "https://" + environ.get("ENV", "dev") + ".orcidhub.org.nz/auth"
 
 # Postgresql connection url
 POSTGRES_PASSWORD = environ.get("POSTGRES_PASSWORD")
