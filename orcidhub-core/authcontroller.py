5d84701fd2c9952a5ebe8a438c4c2f9d274fde5e
Rad Cirskis <nad2000@gmail.com>
2017-03-14 14:41:37 +1300

    improved messaging, + logout after failed attempt to link to ORCiD

--- orcidhub-core/authcontroller.py
+++ orcidhub-core/authcontroller.py
@@ -66,8 +66,8 @@ def login():
             return render_template("base.html", userName=request.headers['Displayname'],
                                    organisationName=request.headers['O'], registerOptions=registerOptions)
         else:
-            flash("Organisation not onboarded", 'warning')
-            return redirect(url_for("index"))
+            flash("Your organisation (%s) is not onboarded" % tuakiri_orgName, 'error')
+            return redirect(url_for("logout"))
     else:
         # return render_template("login.html")
         return redirect(url_for("index"))
