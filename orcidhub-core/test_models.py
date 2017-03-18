340c8d9f7b2eb554d414fc2acb2835a2e957fb9f
Rad Cirskis <nad2000@gmail.com>
2017-03-19 00:36:32 +1300

    fix to get it running in the container

--- orcidhub-core/test_models.py
+++ orcidhub-core/test_models.py
@@ -22,36 +22,36 @@ class ModelTestCase(TestCase):
             orcid_client_id="client-%d" % i,
             orcid_secret="secret-%d" % i,
             confirmed=(i % 2 == 0))
-            for i in range(100))).execute()
+            for i in range(10))).execute()
 
         User.insert_many((dict(
             name="Test User #%d" % i,
             first_name="Test_%d" % i,
             last_name="User_%d" % i,
-            email="user%d@org%d.org.nz" % (i, i * 42 % 100),
+            email="user%d@org%d.org.nz" % (i, i * 4 % 10),
             edu_person_shared_token="EDU PERSON SHARED TOKEN #%d" % i,
             confirmed=(i % 3 != 0),
             roles=Role.SUPERUSER if i % 42 == 0 else Role.ADMIN if i % 13 == 0 else Role.RESEARCHER)
-            for i in range(600))).execute()
+            for i in range(60))).execute()
 
         UserOrg.insert_many((dict(
             is_admin=((u + o) % 23 == 0),
             user=u,
-            org=o) for (u, o) in product(range(2, 600, 43), range(2, 100, 3)))).execute()
+            org=o) for (u, o) in product(range(2, 60, 4), range(2, 10)))).execute()
 
         UserOrg.insert_many((dict(
             is_admin=True,
-            user=42,
-            org=o) for o in range(1, 101))).execute()
+            user=43,
+            org=o) for o in range(1, 11))).execute()
 
     def test_user_org_link(self):
         with test_database(_db, (User, Organisation, UserOrg,)):
             self.create_test_data()
-            assert User.select().count() == 600
-            assert Organisation.select().count() == 100
-            assert User.get(id=42).admin_for.count() == 100
+            assert User.select().count() == 60
+            assert Organisation.select().count() == 10
+            assert User.get(id=43).admin_for.count() == 10
             assert User.get(id=1).admin_for.count() == 0
-            assert User.get(id=45).admin_for.count() > 0
+            assert User.get(id=42).admin_for.count() > 0
             assert User.get(id=2).organisations.count() > 0
             assert Organisation.get(id=1).admins.count() == 1
             assert Organisation.get(id=5).users.count() > 0
