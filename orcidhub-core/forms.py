c59eb5ebd80a7b333c2938844df0f80652128fd8
Rad Cirskis <nad2000@gmail.com>
2017-03-26 02:27:56 +1300

    [skip ci]...

--- orcidhub-core/forms.py
+++ orcidhub-core/forms.py
@@ -7,7 +7,7 @@ class EmploymentForm(FlaskForm):
     employer = StringField("Institution/employer", [validators.required()])
     city = StringField("City", [validators.required()])
     state = StringField("State/region", [validators.required()])
-    country = SelectField("Country", [validators.required()], choices=((c.alpha_2, c.name) for c in countries))
+    country = SelectField("Country", [validators.required()], choices=[(c.alpha_2, c.name) for c in countries])
     department = StringField("Department")
     role = StringField("Role/title")
     start_date = DateField("Start date", format="%Y-%m-%d")
