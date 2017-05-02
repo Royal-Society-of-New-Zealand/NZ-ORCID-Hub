5fbeda14586432507f81dd3f71d93593642af5b3
Rad Cirskis <nad2000@gmail.com>
2017-05-02 20:53:59 +1200

    added a command for data pre-loading...

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from application import app, db
 from authcontroller import *  # noqa: F401, F403
 from views import *  # noqa: F401, F403
 import initializedb
+import click
 
 
 # TODO: connection should be managed explicitely
@@ -41,6 +42,14 @@ def after_request(response):
 def initdb():
     """Initialize the database."""
     initializedb.initdb()
+
+
+@app.cli.command()
+@click.argument('filename')
+def load_org_info(filename):
+    """Pre-loads organisation data."""
+    row_count = models.OrgInfo.load_from_csv(filename)
+    print("Loaded %d records" % row_count)
 
 
 if app.debug:
