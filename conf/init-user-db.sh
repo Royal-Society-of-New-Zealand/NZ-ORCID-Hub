0529f82fde1ca33bf36c506059e67cb906a305fd
Rad Cirskis <nad2000@gmail.com>
2017-06-14 14:54:19 +1200

    [skip ci] ...

--- conf/init-user-db.sh
+++ conf/init-user-db.sh
@@ -6,13 +6,17 @@ cat >>/var/lib/postgresql/data/postgresql.conf <<EOF
 wal_level = logical  # Adds information necessary to support logical decoding
 wal_compression = on  # Compresses full-page writes written in WAL file.
 archive_mode = on  # Allows archiving of WAL files using archive_command.
-archive_command = 'test ! -f /backup/%f.bz2 && bzip2 -c %p >/backup/%f.bz2 && mv /backup/%f.bz2 /archive/'
+archive_command = 'test ! -f /archive/%f.bz2 && bzip2 -c %p >/backup/%f.bz2 && mv /backup/%f.bz2 /archive/'
 archive_timeout = 3600
 max_wal_senders = 5  # Sets the maximum number of simultaneously running WAL sender processes.
 
 #hot_standby = 'on'  # uncomment on "slave" DB server and create recovery.conf:
-##restore_command = 'test -f /archive/%f.bz2 && bzip2 -c -d /archive/%f.bz2 >%p'
-##primary_conninfo = 'host=MASTER_SERVER_IP port=5432 user=postgres'
+EOF
+
+cat >>/var/lib/postgresql/data/_recovery.conf <<EOF
+# rename this file to recovery.conf and change master DB server IP address:
+restore_command = 'test -f /archive/%f.bz2 && bzip2 -c -d /archive/%f.bz2 >%p'
+primary_conninfo = 'host=MASTER_SERVER_IP port=5432 user=postgres'
 EOF
 
 cat >>/var/lib/postgresql/data/pg_hba.conf <<EOF
