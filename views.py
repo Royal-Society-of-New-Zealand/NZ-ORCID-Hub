742dc7151d4264b47db70ec101fda47ebf951b4e
Rad Cirskis <nad2000@gmail.com>
2017-06-16 12:24:23 +1200

    [skip ci] ...

--- views.py
+++ views.py
@@ -16,7 +16,7 @@ from application import admin, app
 from config import ORCID_BASE_URL, SCOPE_ACTIVITIES_UPDATE
 from forms import BitmapMultipleValueField, OrgInfoForm, RecordForm
 from login_provider import roles_required
-from models import PartialDate as PD
+from models import PartialDate as PD, db
 from models import (OrcidApiCall, OrcidToken, Organisation, OrgInfo, Role, User,
                     UserOrgAffiliation)
 # NB! Should be disabled in production
@@ -294,37 +294,43 @@ def edit_section_record(user_id, put_code=None, section_type="EMP"):
                 disambiguated_organization_identifier=org.name, disambiguation_source=org.name)
         try:
             if put_code:
-                # TODO: We can uncomment the below swagger employment update call,
-                # Once the bug fix (in update employment functionality) related to put code is done from ORCID side
-                # api_instance.update_employment(user.orcid, put_code, body=employment)
-                # try:
-                client = OAuth2Session(
-                    org.orcid_client_id,
-                    token={"access_token": orcid_client.configuration.access_token})
-
-                headers = {
-                    'Accept': 'application/vnd.orcid+json',
-                    'Content-type': 'application/vnd.orcid+json'
-                }
-                data = rec.to_dict()
-                data['put-code'] = int(put_code)
-                temp = json.dumps(data).replace('_', '-')
-                data = json.loads(temp)
-                apitype = ""
+                rec.put_code = put_code
                 if section_type == "EMP":
-                    apitype = "employment"
+                    api_response = api_instance.update_employment(user.orcid, body=rec)
                 else:
-                    apitype = "education"
-                resp = client.put(
-                    url="https://api.sandbox.orcid.org/v2.0/" + user.orcid + "/" + apitype + "/" +
-                    str(put_code),
-                    json=data,
-                    headers=headers)
-                if resp.status_code == 200:
-                    flash("Record details has been u" "pdated successfully!", "success")
-                else:
-                    flash("You are not authorised to update this record!", "danger")
-                    return redirect(_url)
+                    api_response = api_instance.update_education(user.orcid, body=rec)
+                # # TODO: We can uncomment the below swagger employment update call,
+                # # Once the bug fix (in update employment functionality) related to put code is done from ORCID side
+                # # api_instance.update_employment(user.orcid, put_code, body=employment)
+                # # try:
+                # client = OAuth2Session(
+                #     org.orcid_client_id,
+                #     token={"access_token": orcid_client.configuration.access_token})
+
+                # headers = {
+                #     'Accept': 'application/vnd.orcid+json',
+                #     'Content-type': 'application/vnd.orcid+json'
+                # }
+                # data = rec.to_dict()
+                # data['put-code'] = int(put_code)
+                # temp = json.dumps(data).replace('_', '-')
+                # data = json.loads(temp)
+                # apitype = ""
+                # if section_type == "EMP":
+                #     apitype = "employment"
+                # else:
+                #     apitype = "education"
+                # resp = client.put(
+                #     url="https://api.sandbox.orcid.org/v2.0/" + user.orcid + "/" + apitype + "/" +
+                #     str(put_code),
+                #     json=data,
+                #     headers=headers)
+                # if resp.status_code == 200:
+                #     flash("Record details has been u" "pdated successfully!", "success")
+                # else:
+                #     flash("You are not authorised to update this record!", "danger")
+                #     return redirect(_url)
+                print(api_response)
             else:
                 if section_type == "EMP":
                     api_response = api_instance.create_employment(user.orcid, body=rec)
@@ -436,3 +442,20 @@ def load_org():
         return redirect(url_for("orginfo.index_view"))
 
     return render_template("orginfo.html", form=form)
+
+
+@app.route("/orcid_api_rep", methods=["GET", "POST"])
+@roles_required(Role.SUPERUSER)
+def orcid_api_rep():
+    """Show ORCID API invocation report."""
+
+    data = db.execute_sql("""
+    WITH rd AS (
+        SELECT date_trunc('minute', call_datetime) AS d, count(*) AS c
+        FROM orcid_api_call
+        GROUP BY date_trunc('minute', call_datetime))
+    SELECT date_trunc('day', d) AS d, max(c) AS c
+    FROM rd GROUP BY DATE_TRUNC('day', d) ORDER BY 1
+    """).fetchall()
+
+    return render_template("orcid_api_call_report.html", data=data)
