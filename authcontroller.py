f80b931ff7ae9b5323698720022a9ebeed8ff788
Rad Cirskis <nad2000@gmail.com>
2017-08-07 15:02:49 +1200

    ..

--- authcontroller.py
+++ authcontroller.py
@@ -343,6 +343,8 @@ def link():
 @app.route("/auth/<path:url>")
 def orcid_callback_proxy(url):
     url = unquote(url)
+    app.logger.info("URL: ", url)
+    app.logger.info("ARGS: ", request.args)
     return redirect(append_qs(url, **request.args))
     # return redirect(url + '?' + urlencode(request.args))
 
