819f1cb6555c4bd02c1100f2f0e84e331ca090b1
Rad Cirskis <nad2000@gmail.com>
2017-08-25 16:10:25 +1200

    [skip ci] added External ID

--- models.py
+++ models.py
@@ -898,6 +898,8 @@ class AffiliationRecord(BaseModel):
 
     task = ForeignKeyField(Task)
     put_code = IntegerField(null=True)
+    external_id = CharField(max_length=100, null=True, verbose_name="External ID",
+        help_text="Recorod identifier used in the data source system.")
     first_name = CharField(max_length=120, null=True)
     last_name = CharField(max_length=120, null=True)
     email = CharField(max_length=120, null=True)
