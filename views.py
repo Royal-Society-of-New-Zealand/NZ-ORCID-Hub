7439a8cbed030f1604e7b2f6407e97c2bcb882fa
Jason Gush <jason.gush@royalsociety.org.nz>
2017-10-17 15:26:09 +1300

    Add whitespace around operator

--- views.py
+++ views.py
@@ -440,7 +440,7 @@ class AffiliationRecordAdmin(AppModelView):
         return super().get_export_name(export_type=export_type)
 
     @action("activate", "Activate for processing",
-            "Are you sure you want to activate the selected records for batch processing?\n\nBy clicking \"OK\" "+
+            "Are you sure you want to activate the selected records for batch processing?\n\nBy clicking \"OK\" " +
             "you are affirming that the affiliations to be written are, to the\n best of your knowledge, correct!")
     def action_activate(self, ids):
         """Batch registraion of users."""
