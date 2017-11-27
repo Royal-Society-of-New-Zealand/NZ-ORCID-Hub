aae08f4d95c81c1c4c7bea12e28479568168ecd9
Roshan Pawar <getconnectedtoroshan@gmail.com>
2017-11-27 14:43:52 +1300

    Changes related to fixing email regex

--- orcid_hub/models.py
+++ orcid_hub/models.py
@@ -32,7 +32,7 @@ from config import DEFAULT_COUNTRY, ENV
 
 from . import app, db
 
-EMAIL_REGEX = re.compile(r"^[_a-z0-9-]+([._a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$")
+EMAIL_REGEX = re.compile(r"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$")
 ORCID_ID_REGEX = re.compile(r"^\d{4}-?\d{4}-?\d{4}-?\d{4}$")
 
 AFFILIATION_TYPES = (
