d8c16bdc2707186f41a6d91e33a37706bc865348
Rad Cirskis <nad2000@gmail.com>
2017-11-28 19:39:41 +1300

    soucre formatting and flake...

--- orcid_hub/orcid_client.py
+++ orcid_hub/orcid_client.py
@@ -9,9 +9,9 @@ from .config import ORCID_API_BASE, SCOPE_READ_LIMITED, SCOPE_ACTIVITIES_UPDATE,
 from flask_login import current_user
 from .models import (OrcidApiCall, Affiliation, OrcidToken, FundingContributor as FundingCont, User
                      as UserModel, ExternalId as ExternalIdModel)
-from orcid_api import (configuration, rest, api_client, MemberAPIV20Api, SourceClientId,
-                            Source, OrganizationAddress, DisambiguatedOrganization, Employment,
-                            Education, Organization)
+from orcid_api import (configuration, rest, api_client, MemberAPIV20Api, SourceClientId, Source,
+                       OrganizationAddress, DisambiguatedOrganization, Employment, Education,
+                       Organization)
 from orcid_api.rest import ApiException
 from time import time
 from urllib.parse import urlparse
