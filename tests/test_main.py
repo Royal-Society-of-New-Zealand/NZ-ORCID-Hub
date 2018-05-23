01fbfb8a85b95013ced43f02b142dd85ebd37180
Roshan Pawar <getconnectedtoroshan@gmail.com>
2018-05-23 19:15:33 +1200

    Minor changes after review

--- tests/test_main.py
+++ tests/test_main.py
@@ -700,20 +700,11 @@ def test_link(request_ctx):
         assert b"<!DOCTYPE html>" in rv.data, "Expected HTML content"
 
 
-def test_faq(request_ctx):
-    """Test faq path traversal security issue."""
-    with request_ctx("/faq") as ctxx:
-        request.args = {'url': '/xyz/sitename'}
-        rv = ctxx.app.full_dispatch_request()
-        assert rv.status_code == 403
-
-
-def test_about(request_ctx):
-    """Test about page path traversal security issue."""
-    with request_ctx("/about") as ctxx:
-        request.args = {'url': '/xyz/sitename'}
-        rv = ctxx.app.full_dispatch_request()
-        assert rv.status_code == 403
+@pytest.mark.parametrize("url", ["/faq", "/about"])
+def test_faq_and_about(client, url):
+    """Test faq and about page path traversal security issue."""
+    rv = client.get(url + "?malicious_code")
+    assert rv.status_code == 403
 
 
 def test_orcid_callback(request_ctx):
