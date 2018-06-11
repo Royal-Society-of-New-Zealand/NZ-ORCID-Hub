3b1c219da073053362efababb87994e2d718412d
Radomirs Cirskis <nad2000@gmail.com>
2018-06-11 20:01:40 +1200

    still need access from the host

--- conf/init-user-db.sh
+++ conf/init-user-db.sh
@@ -30,6 +30,7 @@ local   replication     postgres                            trust
 host    replication     postgres        34.225.18.251/32    trust
 host    all             all             34.225.18.251/32    trust
 host    orcidhub        orcidhub        $(hostname -I)/24   trust
+host    all             all             gateway             trust
 EOF
 
 createdb -U "$POSTGRES_USER" orcidhub
