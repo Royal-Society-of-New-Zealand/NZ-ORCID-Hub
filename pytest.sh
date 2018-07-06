11ef4a8bece6d034a5dd9049067a195b1d88a21a
Rad Cirskis <nad2000@gmail.com>
2018-07-06 17:11:33 +1200

    fixed tests

--- pytest.sh
+++ pytest.sh
@@ -10,4 +10,7 @@ export LANG=en_US.UTF-8
 ##   flask process
 export DATABASE_URL="sqlite:///:memory:"
 export EXTERNAL_SP=''
+[ -z $RQ_REDIS_URL ] && RQ_REDIS_URL=redis://redis:6379/0
+export RQ_REDIS_URL
+
 pytest --ignore=venv --ignore=orcid_api -v --cov-config .coveragerc  --cov . tests $@
