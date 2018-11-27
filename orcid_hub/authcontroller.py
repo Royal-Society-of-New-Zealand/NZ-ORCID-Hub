2ae88cff2fd0900efd29a9027ac3d00d18f40ca2
Roshan Pawar <getconnectedtoroshan@gmail.com>
2018-11-27 17:22:22 +1300

    Minor bug fix for enforcing email address to be in lower case and fixing sentry warning.

--- orcid_hub/authcontroller.py
+++ orcid_hub/authcontroller.py
@@ -917,9 +917,6 @@ def orcid_login(invitation_token=None):
             if OrcidToken.select().where(OrcidToken.user == user, OrcidToken.org == org).exists():
                 flash("You have already given permission, you can simply login on orcidhub",
                       "warning")
-                app.logger.warning(
-                    f"The User {user.email} from {org.name} organisation has already given permission, "
-                    f"but still was trying to login via invitation link.")
                 return redirect(url_for("index"))
 
             try:
