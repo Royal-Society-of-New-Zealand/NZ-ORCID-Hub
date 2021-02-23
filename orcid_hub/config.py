8cdb24c8d642a9bea0d590461120ec14e1acc31d
Guy Halse <guy@tenet.ac.za>
2021-02-23 14:42:23 +0200

    Make it possible to override the SAML attribute map

--- orcid_hub/config.py
+++ orcid_hub/config.py
@@ -68,6 +68,15 @@ SEED_HUB_ADMIN = getenv("SEED_HUB_ADMIN", "rad42@mailinator.com")
 
 # External Shibboleth SP login URL (e.g., https://test.orcidhub.org.nz/Tuakiri/login)
 EXTERNAL_SP = getenv("EXTERNAL_SP") if ENV != "prod" else None
+# Attribute map for external SP
+EXTERNAL_SP_ATTR_EPPN = getenv("EXTERNAL_SP_ATTR_EPPN", "Eppn")
+EXTERNAL_SP_ATTR_SN = getenv("EXTERNAL_SP_ATTR_SN", "Sn")
+EXTERNAL_SP_ATTR_GIVENNAME = getenv("EXTERNAL_SP_ATTR_GIVENNAME", "Givenname")
+EXTERNAL_SP_ATTR_MAIL = getenv("EXTERNAL_SP_ATTR_MAIL", "Mail")
+EXTERNAL_SP_ATTR_DISPLAYNAME = getenv("EXTERNAL_SP_ATTR_DISPLAYNAME", "Displayname")
+EXTERNAL_SP_ATTR_ORG = getenv("EXTERNAL_SP_ATTR_ORG", "O")
+EXTERNAL_SP_ATTR_AFFILIATION = getenv("EXTERNAL_SP_ATTR_AFFILIATION", "Unscoped-Affiliation")
+EXTERNAL_SP_ATTR_ORCID = getenv("EXTERNAL_SP_ATTR_ORCID", "Orcid-Id")
 
 DEFAULT_COUNTRY = "NZ"
 
