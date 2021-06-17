Royal-Society-of-New-Zealand/NZ-ORCID-Hub

commit d0e31c5995693f5e0add791264420c1ac181d24a

Genericize Tuakiri -> saml

orcid_hub/authcontroller.py
@@ -102,7 +102,7 @@ def utility_processor():  # noqa: D202
         rv = cache.get("onboarded_organisations")
         if not rv:
             rv = list(
-                Organisation.select(Organisation.name, Organisation.tuakiri_name).where(
+                Organisation.select(Organisation.name, Organisation.saml_name).where(
                     Organisation.confirmed.__eq__(True)
                 )
             )
@@ -112,7 +112,7 @@ def utility_processor():  # noqa: D202
     def orcid_login_url():
         return url_for("orcid_login", next=get_next_url())
 
-    def tuakiri_login_url():
+    def saml_login_url():
         _next = get_next_url()
         external_sp = app.config.get("EXTERNAL_SP")
         if external_sp:
@@ -162,7 +162,7 @@ def utility_processor():  # noqa: D202
 
     return dict(
         orcid_login_url=orcid_login_url,
-        tuakiri_login_url=tuakiri_login_url,
+        saml_login_url=saml_login_url,
         onboarded_organisations=onboarded_organisations,
         current_task=current_task,
         current_record=current_record,
@@ -199,7 +199,7 @@ def faq():
 
 
 @app.route("/sso/sp")
-@app.route("/Tuakiri/SP")
+@app.route("/saml/SP")
 def shib_sp():
     """Remote Shibboleth authenitication handler.
 
@@ -233,14 +233,14 @@ def get_attributes(key):
     return data, 200, {"Content-Type": "application/octet-stream"}
 
 
-@app.route("/sso/login", endpoint="sso-login")
-@app.route("/Tuakiri/login")
+@app.route("/sso/login", endpoint="saml-login")
+@app.route("/saml/login")
 def handle_login():
     """Shibboleth and Rapid Connect authenitcation handler.
 
     The (Apache) location should requier authentication using Shibboleth, e.g.,
 
-    <Location /Tuakiri>
+    <Location /saml>
         AuthType shibboleth
         ShibRequireSession On
         require valid-user
@@ -312,19 +312,19 @@ def handle_login():
             )
 
     except Exception as ex:
-        app.logger.exception("Failed to login via TUAKIRI.")
+        app.logger.exception("Failed to log in at your home institution.")
         abort(500, ex)
 
     try:
         org = Organisation.get(
-            (Organisation.tuakiri_name == shib_org_name) | (Organisation.name == shib_org_name)
+            (Organisation.saml_name == shib_org_name) | (Organisation.name == shib_org_name)
         )
     except Organisation.DoesNotExist:
-        org = Organisation(tuakiri_name=shib_org_name)
+        org = Organisation(saml_name=shib_org_name)
         # try to get the official organisation name:
         try:
             org_info = OrgInfo.get(
-                (OrgInfo.tuakiri_name == shib_org_name) | (OrgInfo.name == shib_org_name)
+                (OrgInfo.saml_name == shib_org_name) | (OrgInfo.name == shib_org_name)
             )
         except OrgInfo.DoesNotExist:
             org.name = shib_org_name
@@ -956,7 +956,7 @@ def onboard_org():
             try:
                 oi = OrgInfo.get(
                     (OrgInfo.email == email)
-                    | (OrgInfo.tuakiri_name == user.organisation.name)
+                    | (OrgInfo.saml_name == user.organisation.name)
                     | (OrgInfo.name == user.organisation.name)
                 )
 
@@ -1047,7 +1047,7 @@ def onboard_org():
                     ).execute()
 
                 org_info = OrgInfo.get(
-                    (OrgInfo.tuakiri_name == organisation.name)
+                    (OrgInfo.saml_name == organisation.name)
                     | (OrgInfo.name == organisation.name)
                 )
                 if organisation.city:
@@ -1386,7 +1386,7 @@ def orcid_login_callback(request):
                 flash(
                     f"The account with ORCID iD {orcid_id} is not known in the Hub. "
                     "Try again when you've linked your ORCID iD with an organisation through either "
-                    "a Tuakiri-mediated log in, or from an organisation's email invitation",
+                    "an institutional log in, or from an organisation's email invitation",
                     "warning",
                 )
                 return redirect(url_for("index"))

orcid_hub/__init__.py
@@ -337,7 +337,7 @@ def create_hub_administrator(email,
 
     org, _ = models.Organisation.get_or_create(name=org_name or "ORCID Hub")
     if internal_org_name:
-        org.tuakiri_name = internal_org_name
+        org.saml_name = internal_org_name
     org.confirmed = True
     org.save()
     models.UserOrg.get_or_create(user=super_user, org=org)

orcid_hub/models.py
@@ -745,7 +745,7 @@ class Organisation(AuditedModel):
     country_choices.insert(0, ("", "Country"))
 
     name = CharField(max_length=100, unique=True, null=True)
-    tuakiri_name = CharField(max_length=80, unique=True, null=True)
+    saml_name = CharField(max_length=80, unique=True, null=True)
     if ENV != "prod":
         orcid_client_id = CharField(max_length=80, null=True)
         orcid_secret = CharField(max_length=80, null=True)
@@ -834,14 +834,14 @@ class Organisation(AuditedModel):
         return self.users.where(UserOrg.is_admin)
 
     def __str__(self):
-        return self.name or self.tuakiri_name
+        return self.name or self.saml_name
 
     def save(self, *args, **kwargs):
         """Handle data consistency validation and saving."""
         if self.is_dirty():
 
             if self.name is None:
-                self.name = self.tuakiri_name
+                self.name = self.saml_name
 
             if self.field_is_updated("tech_contact") and self.tech_contact:
                 if not self.tech_contact.has_role(Role.TECHNICAL):
@@ -1078,7 +1078,7 @@ class OrgInfo(BaseModel):
     """Preloaded organisation data."""
 
     name = CharField(max_length=100, unique=True, help_text="Organisation name")
-    tuakiri_name = CharField(max_length=100, unique=True, null=True, help_text="TUAKIRI Name")
+    saml_name = CharField(max_length=100, unique=True, null=True, help_text="SAML Name")
     title = CharField(null=True, help_text="Contact Person Tile")
     first_name = CharField(null=True, help_text="Contact Person's First Name")
     last_name = CharField(null=True, help_text="Contact Person's Last Name")
@@ -1134,7 +1134,7 @@ class OrgInfo(BaseModel):
                 "city",
                 "(common:)?disambiguated.*identifier",
                 "(common:)?disambiguation.*source",
-                r"tuakiri\s*(name)?",
+                r"saml\s*(name)?",
             )
         ]
 
@@ -1174,7 +1174,7 @@ class OrgInfo(BaseModel):
             oi.city = val(row, 9)
             oi.disambiguated_id = val(row, 10)
             oi.disambiguation_source = val(row, 11)
-            oi.tuakiri_name = val(row, 12)
+            oi.saml_name = val(row, 12)
 
             oi.save()
 

orcid_hub/views.py
@@ -481,7 +481,7 @@ class OrganisationAdmin(AppModelView):
     form_excluded_columns.append("logo")
     column_searchable_list = (
         "name",
-        "tuakiri_name",
+        "saml_name",
         "city",
     )
     form_ajax_refs = {
@@ -517,14 +517,14 @@ class OrgInfoAdmin(AppModelView):
     can_export = True
     column_searchable_list = (
         "name",
-        "tuakiri_name",
+        "saml_name",
         "city",
         "first_name",
         "last_name",
         "email",
     )
     form_rules = [
-        rules.FieldSet(["name", "tuakiri_name"], "Naming"),
+        rules.FieldSet(["name", "saml_name"], "Naming"),
         rules.FieldSet(["title", "first_name", "last_name", "role", "email", "phone", "is_public"],
                        "Technical Contact"),
         rules.FieldSet(["country", "city"], "Address"),
@@ -542,7 +542,7 @@ class OrgInfoAdmin(AppModelView):
                     org_name=oi.name,
                     email=oi.email,
                     tech_contact=True,
-                    via_orcid=(False if oi.tuakiri_name else True),
+                    via_orcid=(False if oi.saml_name else True),
                     first_name=oi.first_name,
                     last_name=oi.last_name,
                     city=oi.city,
@@ -576,7 +576,7 @@ class OrcidTokenAdmin(AppModelView):
             "page_size": 5
         },
         "org": {
-            "fields": (Organisation.name, Organisation.tuakiri_name),
+            "fields": (Organisation.name, Organisation.saml_name),
             "page_size": 5
         },
     }
@@ -3203,7 +3203,7 @@ def register_org(org_name,
         except OrgInfo.DoesNotExist:
             pass
         else:
-            org.tuakiri_name = org_info.tuakiri_name
+            org.saml_name = org_info.saml_name
 
         try:
             org.save()

commit 644c21550511d9f254718d12ccf73ac915056816

Fix error caused by d0e31c5995693f5e0add791264420c1ac181d24a

orcid_hub/authcontroller.py
@@ -234,7 +234,7 @@ def get_attributes(key):
     return data, 200, {"Content-Type": "application/octet-stream"}
 
 
-@app.route("/sso/login", endpoint="saml-login")
+@app.route("/sso/login", endpoint="sso-login")
 @app.route("/saml/login")
 def handle_login():
     """Shibboleth and Rapid Connect authenitcation handler.