e1efa63da456d2619f36388484cdfe4ac5ee4506
Rad Cirskis <nad2000@gmail.com>
2021-07-14 11:58:18 +1200

    reduce memory for compression and free memory before running backups

--- backup-db.sh
+++ backup-db.sh
@@ -3,10 +3,11 @@
 TS_LABEL=$(date +%FT%s)
 PATH=/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:$HOME/.local/bin:$HOME/bin:$PATH:/usr/local/bin
 
+sudo bash -c 'sync; echo 3 > /proc/sys/vm/drop_caches'
 [ ! -f docker-compose.yml ] && cd $HOME
 docker-compose exec -T db psql -U postgres -c "VACUUM FULL ANALYZE;"
 docker-compose exec -T db psql -U postgres -c "SELECT pg_start_backup('$TS_LABEL', false);"
 # tar cjf ./backup/$TS_LABEL.tar.bz2 ./pgdata ; mv ./backup/$TS_LABEL.tar.bz2 ./archive/
-XZ_OPT="-9 --memory=135000000" tar cJf ./backup/$TS_LABEL.tar.xz ./pgdata ; mv ./backup/$TS_LABEL.tar.xz ./archive/
+XZ_OPT="-9 --memory=100000000" tar cJf ./backup/$TS_LABEL.tar.xz ./pgdata ; mv ./backup/$TS_LABEL.tar.xz ./archive/
 docker-compose exec -T db psql -U postgres -c "SELECT pg_stop_backup();"
 find ./archive -mtime +10 -exec rm {} \;
