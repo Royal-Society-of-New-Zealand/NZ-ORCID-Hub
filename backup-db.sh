c063c59981b28fb3b003d06d379ada0ea3be6b9c
Rad Cirskis <nad2000@gmail.com>
2023-01-11 16:32:10 +0900

    increased statement timeout and added vacuuming of the application DB

--- backup-db.sh
+++ backup-db.sh
@@ -6,6 +6,7 @@ PATH=/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:$HOME/.local/bin
 sudo bash -c 'sync; echo 3 > /proc/sys/vm/drop_caches'
 [ ! -f docker-compose.yml ] && cd $HOME
 docker-compose exec -T db psql -U postgres -c "VACUUM FULL ANALYZE;"
+docker-compose exec -T db psql -U postgres -c "VACUUM FULL ANALYZE;" -d orcidhub
 docker-compose exec -T db psql -U postgres -c "SELECT pg_start_backup('$TS_LABEL', false);"
 # tar cjf ./backup/$TS_LABEL.tar.bz2 ./pgdata ; mv ./backup/$TS_LABEL.tar.bz2 ./archive/
 XZ_OPT="-9 --memory=100000000" tar cJf ./backup/$TS_LABEL.tar.xz ./pgdata ; mv ./backup/$TS_LABEL.tar.xz ./archive/
