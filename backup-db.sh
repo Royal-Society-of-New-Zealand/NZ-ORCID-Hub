33cd471bfff974f17c9ffbdae2eea08fc5f60e1d
ORCID HUB DEV <ec2-user@dev.orcidhub.org.nz>
2023-03-08 07:22:00 +0000

    moved to external PostgreSQL

--- backup-db.sh
+++ backup-db.sh
@@ -7,8 +7,11 @@ sudo bash -c 'sync; echo 3 > /proc/sys/vm/drop_caches'
 [ ! -f docker-compose.yml ] && cd $HOME
 docker-compose exec -T db psql -U postgres -c "VACUUM FULL ANALYZE;"
 docker-compose exec -T db psql -U postgres -c "VACUUM FULL ANALYZE;" -d orcidhub
-docker-compose exec -T db psql -U postgres -c "SELECT pg_start_backup('$TS_LABEL', false);"
+# docker-compose exec -T db psql -U postgres -c "SELECT pg_start_backup('$TS_LABEL', false);"
+docker-compose exec -T db psql -U postgres -c "SELECT pg_backup_start('$TS_LABEL');"
 # tar cjf ./backup/$TS_LABEL.tar.bz2 ./pgdata ; mv ./backup/$TS_LABEL.tar.bz2 ./archive/
-XZ_OPT="-9 --memory=100000000" tar cJf ./backup/$TS_LABEL.tar.xz ./pgdata ; mv ./backup/$TS_LABEL.tar.xz ./archive/
-docker-compose exec -T db psql -U postgres -c "SELECT pg_stop_backup();"
-find ./archive -mtime +10 -exec rm {} \;
+sudo XZ_OPT="-9 --memory=100000000" tar cJf ./backup/$TS_LABEL.tar.xz ./pgdata 
+sudo mv ./backup/$TS_LABEL.tar.xz ./archive/
+# docker-compose exec -T db psql -U postgres -c "SELECT pg_stop_backup();"
+docker-compose exec -T db psql -U postgres -c "SELECT pg_backup_stop();"
+sudo find ./archive -mtime +3 -exec rm {} \;
