05dcf77ba0ad997613f657b8afa7f8715e1f7758
ORCID HUB DEV <ec2-user@dev.orcidhub.org.nz>
2023-03-29 03:26:42 +0000

    backup of the host DB cluster

--- backup-db.sh
+++ backup-db.sh
@@ -3,15 +3,36 @@
 TS_LABEL=$(date +%FT%s)
 PATH=/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:$HOME/.local/bin:$HOME/bin:$PATH:/usr/local/bin
 
-sudo bash -c 'sync; echo 3 > /proc/sys/vm/drop_caches'
 [ ! -f docker-compose.yml ] && cd $HOME
-docker-compose exec -T db psql -U postgres -c "VACUUM FULL ANALYZE;"
-docker-compose exec -T db psql -U postgres -c "VACUUM FULL ANALYZE;" -d orcidhub
-# docker-compose exec -T db psql -U postgres -c "SELECT pg_start_backup('$TS_LABEL', false);"
-docker-compose exec -T db psql -U postgres -c "SELECT pg_backup_start('$TS_LABEL');"
-# tar cjf ./backup/$TS_LABEL.tar.bz2 ./pgdata ; mv ./backup/$TS_LABEL.tar.bz2 ./archive/
-sudo XZ_OPT="-9 --memory=100000000" tar cJf ./backup/$TS_LABEL.tar.xz ./pgdata 
+if docker-compose -q db &>/dev/null ; then
+  docker-compose exec -T db psql -U postgres -c "VACUUM FULL ANALYZE;"
+  docker-compose exec -T db psql -U postgres -c "VACUUM FULL ANALYZE;" -d orcidhub
+  # docker-compose exec -T db psql -U postgres -c "SELECT pg_start_backup('$TS_LABEL', false);"
+  docker-compose exec -T db psql -U postgres -c "SELECT pg_backup_start('$TS_LABEL');"
+  sudo bash -c 'sync; echo 3 > /proc/sys/vm/drop_caches'
+  # tar cjf ./backup/$TS_LABEL.tar.bz2 ./pgdata ; mv ./backup/$TS_LABEL.tar.bz2 ./archive/
+  sudo XZ_OPT="-9 --memory=100000000" tar cJf ./backup/$TS_LABEL.tar.xz ./pgdata 
+else
+  # Backup host DB cluster:
+  cd $HOME
+  export PGUSER=postgres
+  # psql -c "VACUUM FULL ANALYZE;"
+  # psql -c "VACUUM FULL ANALYZE;" -d orcidhub
+  # psql -c "VACUUM FULL ANALYZE;" -d orcidhub_dev
+  # psql -c "VACUUM FULL ANALYZE;" -d orcidhub_test
+  # psql -U postgres -c "SELECT pg_start_backup('$TS_LABEL', false);"
+  # vacuumdb -a -z
+  psql -c "SELECT pg_start_backup('$TS_LABEL');"
+  # tar cjf ./backup/$TS_LABEL.tar.bz2 ./pgdata ; mv ./backup/$TS_LABEL.tar.bz2 ./archive/
+  sudo bash -c 'sync; echo 3 > /proc/sys/vm/drop_caches'
+  sudo XZ_OPT="-9 --memory=1000000000" tar cJf ./backup/$TS_LABEL.tar.xz -C $(psql -t -c "SHOW data_directory;")/.. ./data 
+fi
 sudo mv ./backup/$TS_LABEL.tar.xz ./archive/
-# docker-compose exec -T db psql -U postgres -c "SELECT pg_stop_backup();"
-docker-compose exec -T db psql -U postgres -c "SELECT pg_backup_stop();"
+if docker-compose -q db &>/dev/null ; then
+  # docker-compose exec -T db psql -U postgres -c "SELECT pg_stop_backup();"
+  docker-compose exec -T db psql -U postgres -c "SELECT pg_backup_stop();"
+else
+  psql -c "SELECT pg_stop_backup();"
+fi
+
 sudo find ./archive -mtime +3 -exec rm {} \;
